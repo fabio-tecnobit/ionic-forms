15f0d245fdb64b01d3bb5281f3a8942bb6c73e8e
Fabio <fabio@tecnobit.com.br>
2017-10-24 21:51:33 -0300

    correcoes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -9,7 +9,9 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class HomePage {
   private myForm: FormGroup;
   constructor(public navCtrl: NavController,
-    private formBuilder: FormBuilder) {
+    private formBuilder: FormBuilder,
+  private alertCtrl: AlertController
+) {
 
     this.myForm = formBuilder.group({
       campo1: [],
@@ -19,14 +21,18 @@ export class HomePage {
 
   }
   onSubmitMyForm(): void {
-
-     let myForm = this.myForm.value;
-     console.log(myForm); 
-     console.log(myForm['campo1']);
-    /*this.exemploForm = this.formBuilder.group({
-      title: ['', Validators.required],
-      description: [''],
-    // });*/
+     let frm = this.myForm.value;
+     let v1 = frm['campo1'];
+     let v2 = frm['campo2'];  
+     let total  =parseInt(v1)+parseInt(v2);   
+     this.showAlert(total.toString());
+  }
+  showAlert(message:string):void{
+    let alert = this.alertCtrl.create({
+      message: message,
+      buttons:['ok']
+    });
+    alert.present();
   }
 
 }
